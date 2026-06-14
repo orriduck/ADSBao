412d59c19c76362740f42db4aafa288a96e18f5b
ruyyi0323 <ruyyi0323@gmail.com>
2026-06-14 18:51:32 -0400

    Track applied New Relic resources in IaC

--- infra/newrelic/outputs.tf
+++ infra/newrelic/outputs.tf
@@ -3,6 +3,11 @@ output "dashboard_guid" {
   value       = newrelic_one_dashboard.adsbao_data_service.guid
 }
 
+output "dashboard_permalink" {
+  description = "New Relic dashboard permalink."
+  value       = newrelic_one_dashboard.adsbao_data_service.permalink
+}
+
 output "alert_policy_id" {
   description = "New Relic alert policy ID."
   value       = newrelic_alert_policy.adsbao_data_service.id
